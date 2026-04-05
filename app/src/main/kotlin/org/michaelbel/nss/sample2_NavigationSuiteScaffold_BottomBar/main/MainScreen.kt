822034d4eab6045e3e3c755e4b62df28836497f7
michaelbel <michael-bel@outlook.com>
2026-04-05 15:19:45 +0300

    Update project

--- app/src/main/kotlin/org/michaelbel/nss/sample2_NavigationSuiteScaffold_BottomBar/main/MainScreen.kt
+++ app/src/main/kotlin/org/michaelbel/nss/sample2_NavigationSuiteScaffold_BottomBar/main/MainScreen.kt
@@ -2,11 +2,14 @@
 
 package org.michaelbel.nss.sample2_NavigationSuiteScaffold_BottomBar.main
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
@@ -16,10 +19,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import org.michaelbel.nss.Tabs
 import org.michaelbel.nss.sample2_NavigationSuiteScaffold_BottomBar.main.about.AboutScreen
 import org.michaelbel.nss.sample2_NavigationSuiteScaffold_BottomBar.main.home.HomeScreen
 import org.michaelbel.nss.sample2_NavigationSuiteScaffold_BottomBar.main.settings.SettingsScreen
-import org.michaelbel.nss.Tabs
 
 @Composable
 fun MainScreen(
@@ -28,56 +32,60 @@ fun MainScreen(
     var selectedTab by rememberSaveable(stateSaver = Tabs.Saver) { mutableStateOf(Tabs.Home) }
 
     NavigationSuiteScaffold(
-        layoutType = NavigationSuiteType.ShortNavigationBarCompact,
-        navigationSuiteItems = {
-            item(
-                selected = selectedTab == Tabs.Home,
-                onClick = { selectedTab = Tabs.Home },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Home,
-                        contentDescription = null
-                    )
-                },
-                label = {
-                    Text(
-                        text = "Home"
-                    )
-                }
-            )
+        navigationItems = {
+            NavigationBar(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                NavigationBarItem(
+                    selected = selectedTab == Tabs.Home,
+                    onClick = { selectedTab = Tabs.Home },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Home,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = "Home"
+                        )
+                    }
+                )
 
-            item(
-                selected = selectedTab == Tabs.Settings,
-                onClick = { selectedTab = Tabs.Settings },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Settings,
-                        contentDescription = null
-                    )
-                },
-                label = {
-                    Text(
-                        text = "Settings"
-                    )
-                }
-            )
+                NavigationBarItem(
+                    selected = selectedTab == Tabs.Settings,
+                    onClick = { selectedTab = Tabs.Settings },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Settings,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = "Settings"
+                        )
+                    }
+                )
 
-            item(
-                selected = selectedTab == Tabs.About,
-                onClick = { selectedTab = Tabs.About },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Info,
-                        contentDescription = null
-                    )
-                },
-                label = {
-                    Text(
-                        text = "About"
-                    )
-                }
-            )
-        }
+                NavigationBarItem(
+                    selected = selectedTab == Tabs.About,
+                    onClick = { selectedTab = Tabs.About },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = "About"
+                        )
+                    }
+                )
+            }
+        },
+        navigationSuiteType = NavigationSuiteType.ShortNavigationBarCompact
     ) {
         when (selectedTab) {
             Tabs.Home -> {
